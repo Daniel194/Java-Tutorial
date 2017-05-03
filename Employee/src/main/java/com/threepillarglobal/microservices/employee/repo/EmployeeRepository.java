9f40909a61ed0d8dc74b1f8a632a9844f1706834
Daniel194 <lungu.daniel94@gmail.com>
2017-05-03 21:50:15 +0300

    Employee

--- Employee/src/main/java/com/threepillarglobal/microservices/employee/repo/EmployeeRepository.java
+++ Employee/src/main/java/com/threepillarglobal/microservices/employee/repo/EmployeeRepository.java
@@ -1,6 +1,8 @@
-package ro.spring.docker;
+package com.threepillarglobal.microservices.employee.repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.threepillarglobal.microservices.employee.model.Employee;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 
