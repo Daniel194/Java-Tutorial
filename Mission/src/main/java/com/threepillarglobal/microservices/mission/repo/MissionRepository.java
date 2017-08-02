8b6458d37428fcc14ff91317366f8c7f09d09640
Daniel194 <lungu.daniel94@gmail.com>
2017-08-03 00:08:14 +0300

    Clean up

--- Mission/src/main/java/com/threepillarglobal/microservices/mission/repo/MissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.threepillarglobal.microservices.mission.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.threepillarglobal.microservices.mission.model.Mission;
-
-public interface MissionRepository extends MongoRepository<Mission, String> {
-
-}
