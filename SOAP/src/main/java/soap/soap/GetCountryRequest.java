41cae83fd40e50beaf08c9cb7aaa8dad0fcfafc6
Daniel194 <lungu.daniel94@gmail.com>
2017-09-03 23:34:31 +0300

    SOAP

---
+++ SOAP/src/main/java/soap/soap/GetCountryRequest.java
@@ -0,0 +1,14 @@
+package soap.soap;
+
+public class GetCountryRequest {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ SOAP/src/main/java/soap/soap/GetCountryResponse.java
@@ -0,0 +1,14 @@
+package soap.soap;
+
+public class GetCountryResponse {
+
+    private Country country;
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
