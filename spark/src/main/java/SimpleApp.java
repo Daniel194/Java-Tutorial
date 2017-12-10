3bab4e9bbb1bfa7b2d3804ed482fb4885ae003c1
Daniel194 <lungu.daniel94@gmail.com>
2017-12-10 22:22:45 +0200

    Spark - Count words

--- spark/src/main/java/SimpleApp.java
+++ spark/src/main/java/SimpleApp.java
@@ -1,8 +1,26 @@
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
+import java.util.Arrays;
+
+/**
+ * Command: /Users/daniellungu/Workspace/spark-2.2.1-bin-hadoop2.7/bin/spark-submit --class "SimpleApp" --master local[4] build/libs/spark-1.0-SNAPSHOT.jar
+ */
 public class SimpleApp {
+
+    private static final String YOUR_SPARK_HOME = "/Users/daniellungu/Workspace/spark-2.2.1-bin-hadoop2.7/";
+    private static final String SOURCE_JAR = "spark/build/libs/spark-1.0-SNAPSHOT.jar";
+
     public static void main(String[] args) {
+        //firstExample();
+        wordCount();
+    }
+
+    private static void firstExample() {
         String logFile = "/Users/daniellungu/Workspace/Java-Tutorial/spark/src/main/resources/test.txt";
         SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
         Dataset<String> logData = spark.read().textFile(logFile).cache();
@@ -14,4 +32,18 @@ public class SimpleApp {
 
         spark.stop();
     }
+
+    private static void wordCount() {
+        JavaSparkContext sc = new JavaSparkContext("local", "Simple App",
+                YOUR_SPARK_HOME, new String[]{SOURCE_JAR});
+        JavaRDD<String> textFile = sc.textFile("file:///Users/daniellungu/Workspace/Java-Tutorial/spark/src/main/resources/test.txt");
+
+        JavaPairRDD<String, Integer> counts = textFile
+                .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((a, b) -> a + b);
+
+        counts.saveAsTextFile("file:///Users/daniellungu/Workspace/Java-Tutorial/spark/src/main/resources/out");
+    }
+
 }
