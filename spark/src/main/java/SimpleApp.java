f37e59632a1333f6640b1e5a6e3212f2a055b0c5
Daniel194 <lungu.daniel94@gmail.com>
2017-12-10 22:29:51 +0200

    Spark - Pi Estimation

--- spark/src/main/java/SimpleApp.java
+++ spark/src/main/java/SimpleApp.java
@@ -5,7 +5,9 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Command: /Users/daniellungu/Workspace/spark-2.2.1-bin-hadoop2.7/bin/spark-submit --class "SimpleApp" --master local[4] build/libs/spark-1.0-SNAPSHOT.jar
@@ -14,10 +16,12 @@ public class SimpleApp {
 
     private static final String YOUR_SPARK_HOME = "/Users/daniellungu/Workspace/spark-2.2.1-bin-hadoop2.7/";
     private static final String SOURCE_JAR = "spark/build/libs/spark-1.0-SNAPSHOT.jar";
+    private static final Integer NUM_SAMPLES = 1000000;
 
     public static void main(String[] args) {
         //firstExample();
-        wordCount();
+        //wordCount();
+        piEstimation();
     }
 
     private static void firstExample() {
@@ -44,6 +48,25 @@ public class SimpleApp {
                 .reduceByKey((a, b) -> a + b);
 
         counts.saveAsTextFile("file:///Users/daniellungu/Workspace/Java-Tutorial/spark/src/main/resources/out");
+    }
+
+    private static void piEstimation() {
+        JavaSparkContext sc = new JavaSparkContext("local", "Simple App",
+                YOUR_SPARK_HOME, new String[]{SOURCE_JAR});
+
+        List<Integer> l = new ArrayList<>(NUM_SAMPLES);
+
+        for (int i = 0; i < NUM_SAMPLES; i++) {
+            l.add(i);
+        }
+
+        long count = sc.parallelize(l).filter(i -> {
+            double x = Math.random();
+            double y = Math.random();
+            return x * x + y * y < 1;
+        }).count();
+
+        System.out.println("Pi is roughly :" + 4.0 * count / NUM_SAMPLES);
     }
 
 }
