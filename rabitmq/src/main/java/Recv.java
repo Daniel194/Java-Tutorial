7e9b28be47353f0654f22682af831b8691745d07
Daniel194 <lungu.daniel94@gmail.com>
2018-01-08 23:41:22 +0200

    Rabbit MQ -> Work Queues

--- rabitmq/src/main/java/Recv.java
+++ rabitmq/src/main/java/Recv.java
@@ -7,26 +7,41 @@ public class Recv {
 
     private final static String QUEUE_NAME = "hello";
 
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
+    }
+
     public static void main(String[] argv) throws java.io.IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        Consumer consumer = new DefaultConsumer(channel) {
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
+
                 System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(" [x] Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
             }
         };
-        channel.basicConsume(QUEUE_NAME, true, consumer);
-
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME, autoAck, consumer);
     }
 
 }
--- rabitmq/src/main/java/Send.java
+++ rabitmq/src/main/java/Send.java
@@ -1,6 +1,7 @@
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 
 import java.util.concurrent.TimeoutException;
 
@@ -12,18 +13,33 @@ public class Send {
 
     private final static String QUEUE_NAME = "hello";
 
-    public static void main(String[] argv) throws java.io.IOException, TimeoutException {
-        String message = "Hello World!";
+    private static String getMessage(String[] strings) {
+        if (strings.length < 1)
+            return "Hello World!";
+        return joinStrings(strings, " ");
+    }
 
+    private static String joinStrings(String[] strings, String delimiter) {
+        int length = strings.length;
+        if (length == 0) return "";
+        StringBuilder words = new StringBuilder(strings[0]);
+        for (int i = 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
+    }
+
+    public static void main(String[] argv) throws java.io.IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 
+        String message = getMessage(argv);
+
+        channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
         System.out.println(" [x] Sent '" + message + "'");
 
         channel.close();
