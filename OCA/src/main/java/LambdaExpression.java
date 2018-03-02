7e38f8216aed89523773f4b9c4da7ef5f3ec859a
Daniel194 <lungu.daniel94@gmail.com>
2018-03-02 09:11:59 +0200

    OCA

---
+++ OCA/src/main/java/LambdaExpression.java
@@ -0,0 +1,17 @@
+import java.util.function.Predicate;
+
+public class LambdaExpression {
+
+    public static void main(String[] args) {
+        System.out.println(test(i -> i == 5));
+        System.out.println(test((i) -> i == 5));
+        System.out.println(test((i) -> {
+            return i == 5;
+        }));
+    }
+
+    private static boolean test(Predicate<Integer> p) {
+        return p.test(5);
+    }
+
+}
