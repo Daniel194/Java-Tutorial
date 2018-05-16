4c74b9775b60b9235ed907d50d7e04197caf1048
Daniel194 <lungu.daniel94@gmail.com>
2018-05-16 07:44:24 +0300

    OCP -> threads

--- OCA/src/main/java/Thread1.java
+++ OCA/src/main/java/Thread1.java
@@ -1,20 +1,23 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Thread1 {
 
-    public static void main(String[] args) {
+    private static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService service = null;
+
         try {
             service = Executors.newSingleThreadExecutor();
-            System.out.println("begin");
-            service.execute(() -> System.out.println("Printing zoo inventory"));
-            service.execute(() -> {
-                for (int i = 0; i < 3; i++)
-                    System.out.println("Printing record: " + i);
+
+            Future<?> result = service.submit(() -> {
+                for (int i = 0; i < 500; i++) Thread1.counter++;
             });
-            service.execute(() -> System.out.println("Printing zoo inventory"));
-            System.out.println("end");
+
+            result.get(10, TimeUnit.SECONDS);
+            System.out.println("Reached!");
+        } catch (TimeoutException e) {
+            System.out.println("Not reached in time");
         } finally {
             if (service != null) service.shutdown();
         }
