e2fc74b3acd32975e6e243cd9b5af4838310878a
Daniel194 <lungu.daniel94@gmail.com>
2018-05-30 09:46:19 +0300

    Hackerrank -> PriorityQueue

--- hackerrank/src/main/java/hackerrank/JavaSort.java
+++ hackerrank/src/main/java/hackerrank/JavaSort.java
@@ -1,50 +1,7 @@
 package hackerrank;
 
-import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
-
-class Student implements Comparable {
-    private int id;
-    private String fname;
-    private double cgpa;
-
-    public Student(int id, String fname, double cgpa) {
-        super();
-        this.id = id;
-        this.fname = fname;
-        this.cgpa = cgpa;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public double getCgpa() {
-        return cgpa;
-    }
-
-    public int compareTo(Object o) {
-        Student s;
-        int result = 0;
-
-        if (o instanceof Student) {
-            s = (Student) o;
-        } else {
-            throw new IllegalArgumentException("Please provide a Student !");
-        }
-
-        result = (int) (s.getCgpa() * 100 - this.cgpa * 100);
-        result += result == 0 ? this.fname.compareTo(s.getFname()) : 0;
-        result += result == 0 ? this.id - s.getId() : 0;
-
-        return result;
-    }
-}
 
 public class JavaSort {
 
---
+++ hackerrank/src/main/java/hackerrank/PriorityQueue.java
@@ -0,0 +1,62 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PriorityQueue {
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Priorities priorities = new Priorities();
+
+    public static void main(String[] args) {
+        int totalEvents = Integer.parseInt(scan.nextLine());
+        List<String> events = new ArrayList<>();
+
+        while (totalEvents-- != 0) {
+            String event = scan.nextLine();
+            events.add(event);
+        }
+
+        List<Student> students = priorities.getStudents(events);
+
+        if (students.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            for (Student st : students) {
+                System.out.println(st.getName());
+            }
+        }
+    }
+}
+
+
+class Priorities {
+    private java.util.PriorityQueue<Student> students;
+
+    Priorities() {
+        students = new java.util.PriorityQueue<>();
+    }
+
+    List<Student> getStudents(List<String> events) {
+        for (String ev : events) {
+            executeEvent(ev.split(" "));
+        }
+
+        return new ArrayList<>(students);
+    }
+
+    private void executeEvent(String[] events) {
+        if ("ENTER".equals(events[0])) {
+            addStudent(events[1], events[2], events[3]);
+        } else if ("SERVED".equals(events[0])) {
+            students.poll();
+        }
+    }
+
+    private void addStudent(String fname, String sCgpa, String sId) {
+        int id = Integer.parseInt(sId);
+        double cgpa = Double.parseDouble(sCgpa);
+        students.add(new Student(id, fname, cgpa));
+    }
+
+}
