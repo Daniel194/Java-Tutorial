a954902c623b393f58c24f33fd9b0913617f113a
Daniel194 <lungu.daniel94@gmail.com>
2018-06-18 23:27:53 +0300

    Design Pattern -> Command pattern - WIP

---
+++ designpattern/src/main/java/command/SimpleRemoteControl.java
@@ -0,0 +1,14 @@
+package command;
+
+public class SimpleRemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void buttonWasPressed() {
+        this.command.execute();
+    }
+
+}
