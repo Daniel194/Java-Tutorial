8f481c670927eec10c33781d7621afdbd3949bb3
Daniel194 <lungu.daniel94@gmail.com>
2018-08-19 22:57:45 +0300

    Java 10

--- java-new-versions/src/Java9.java
+++ java-new-versions/src/Java9.java
@@ -20,3 +20,17 @@ public class Java9 {
     }
 
 }
+
+
+interface Card {
+
+    private Long createCardID() {
+        // Method implementation goes here.
+        return 1L;
+    }
+
+    private static void displayCardDetails() {
+        // Method implementation goes here.
+    }
+
+}
